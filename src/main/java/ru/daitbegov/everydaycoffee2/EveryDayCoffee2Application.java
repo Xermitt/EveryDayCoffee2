5378583a3c2c57854aabb49d7bc931a2f75964c2
Muhammadsaygid <daitbegov04@bk.ru>
2024-06-10 21:30:14 +0300

    git init commit

---
+++ src/main/java/ru/daitbegov/everydaycoffee2/EveryDayCoffee2Application.java
@@ -0,0 +1,13 @@
+package ru.daitbegov.everydaycoffee2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryDayCoffee2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryDayCoffee2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/daitbegov/everydaycoffee2/EveryDayCoffee2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daitbegov.everydaycoffee2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryDayCoffee2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
